37ff80904d244eb97d18434c88486694cbb1f614
Rodolpho Reis <rodolphoreis@live.com>
2024-07-15 16:29:50 +0100

    feat: add useState

--- src/pages/Main/index.jsx
+++ src/pages/Main/index.jsx
@@ -1,7 +1,10 @@
 import { FaGithub, FaPlus } from "react-icons/fa";
 import { Container, Form, SubmitButton } from "./styles";
+import { useState } from "react";
 
 export default function Main() {
+  const [newRepo, setNewRepo] = useState("");
+
   return (
     <Container>
       <h1>
@@ -10,7 +13,13 @@ export default function Main() {
       </h1>
 
       <Form onSubmit={() => {}}>
-        <input type="text" placeholder="Adicionar Repositório" />
+        <input
+          type="text"
+          name="newRepo"
+          value={newRepo}
+          onChange={(event) => setNewRepo(event.target.value)}
+          placeholder="Adicionar Repositório"
+        />
         <SubmitButton type="submit" style={{ background: "#0D2636" }}>
           <FaPlus size={14} color="white" />
         </SubmitButton>
