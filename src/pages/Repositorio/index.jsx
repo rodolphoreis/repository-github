00e70974baba0e5114d6dd92f8a56b792b38b441
Rodolpho Reis <rodolphoreis@live.com>
2024-07-17 20:04:56 +0100

    feat(api): add async loading of repository data and issues

--- src/pages/Repositorio/index.jsx
+++ src/pages/Repositorio/index.jsx
@@ -1,12 +1,29 @@
 import { useParams } from "react-router-dom";
+import { Container } from "./styles";
+import { useEffect } from "react";
+import api from "../../services/api";
 
 export default function Repositorio() {
   const { repositorio } = useParams();
+
+  useEffect(() => {
+    async function load() {
+      const [repositoriosData, issuesData] = await Promise.all([
+        api.get(`repos/${repositorio}`),
+        api.get(`repos/${repositorio}/issues`, {
+          params: {
+            state: "open",
+            per_page: 5,
+          },
+        }),
+      ]);
+    }
+    load();
+  }, [repositorio]);
+
   return (
-    <>
-      <div>
-        <h1 style={{ color: "white" }}>Repositorio {repositorio}</h1>
-      </div>
-    </>
+    <Container>
+      <h1 style={{ color: "white" }}>{repositorio}</h1>
+    </Container>
   );
 }
